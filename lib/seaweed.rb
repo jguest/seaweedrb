f4b12b7bfee7f779b88209ed63b218aba503dd93
John Guest <guest.john88@gmail.com>
2015-09-25 11:45:06 -0700

    some documentation

--- lib/seaweed.rb
+++ lib/seaweed.rb
@@ -1,13 +1,30 @@
 module Seaweed
 
+  # supply seaweed-fs master connection info
+  #
+  # @param String host
+  # @param String port
+  # @param Hash http_options
+
   def self.connect(host: "localhost", port: 9333, http_options: {})
     Seaweed::HTTP.configure http_options
     Seaweed::Master.connect host: host, port: port
   end
 
+  # get the system status
+  # https://github.com/chrislusf/seaweedfs/wiki/API#check-system-status
+  #
+  # @return Hash
+
   def self.status
     Seaweed::Master.status
   end
+
+  # upload a new file
+  # https://github.com/chrislusf/seaweedfs/wiki/API#upload-file
+  #
+  # @param String path
+  # @return Seaweed::File
 
   def self.upload(path)
     location = Seaweed::Master.dir_assign!
@@ -15,7 +32,13 @@ module Seaweed
     file.upload!
   end
 
-  def self.find(fid, http_options: nil)
+  # find a file by file id
+  # https://github.com/chrislusf/seaweedfs/wiki/API#lookup-volume
+  #
+  # @param String fid
+  # @return Seaweed::File
+
+  def self.find(fid)
     location = Seaweed::Master.dir_lookup fid.split(",")[0]
     Seaweed::File.new fid, volume_url: location[:url]
   end
