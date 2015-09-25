37ece8dbd5d9b1e3616308d0c1fd3be66a635eb4
John Guest <guest.john88@gmail.com>
2015-09-25 14:10:38 -0700

    more documentation

--- lib/seaweed/file.rb
+++ lib/seaweed/file.rb
@@ -1,6 +1,21 @@
 class Seaweed::File
 
+  # a seaweed-fs file object
+  #
+  # String id
+  # String name
+  # String volume_id
+  # String attachement
+  # String size
+
   attr_reader :id, :name, :volume_id, :volume_url, :attachment, :size
+
+  # create a new file
+  #
+  # @param String fid
+  # @param String volume_url
+  # @param String attachment
+  # @return self
 
   def initialize(fid, volume_url: nil, attachment: nil)
     @id = fid
@@ -8,6 +23,9 @@ class Seaweed::File
     @volume_url = volume_url
     @attachment = attachment
   end
+
+  # upload the file attachment
+  # @return self
 
   def upload!
     response = Seaweed::HTTP.put url, file: File.new(@attachment, 'rb')
@@ -17,18 +35,30 @@ class Seaweed::File
     self
   end
 
+  # delete the uploaded file
+  # @return Boolean
+
   def delete!
     res = Seaweed::HTTP.delete url
     !(Seaweed::HTTP.parse(res)[:size].nil?)
   end
 
+  # read the uploaded file contents
+  # @return String
+
   def read
     Seaweed::HTTP.get url
   end
 
+  # the file's short url in the volume server
+  # @return String
+
   def url
     "#{@volume_url}/#{@id}"
   end
+
+  # the file's most verbose url in the volume server
+  # @return String
 
   def pretty_url
     "#{@volume_url}/#{@volume_id}/#{@key}#{@cookie}/#{@name}"
--- lib/seaweed/master.rb
+++ lib/seaweed/master.rb
@@ -1,24 +1,50 @@
 class Seaweed::Master
   class << self
 
+    # supply seaweed-fs master connection info
+    #
+    # @param String host
+    # @param String port
+
     def connect(host: "localhost", port: 9333)
       @base_url = "http://#{host}:#{port}"
     end
+
+    # get the system status
+    # https://github.com/chrislusf/seaweedfs/wiki/API#check-system-status
+    #
+    # @return Hash
 
     def status
       res = Seaweed::HTTP.get "#{@base_url}/cluster/status?pretty=y"
       Seaweed::HTTP.parse res
     end
 
+    # assign a key file
+    # https://github.com/chrislusf/seaweedfs/wiki/API#assign-a-file-key
+    #
+    # @return Hash
+
     def dir_assign!
       res = Seaweed::HTTP.post "#{@base_url}/dir/assign", {}
       Seaweed::HTTP.parse res
     end
 
+    # lookup a volume
+    # https://github.com/chrislusf/seaweedfs/wiki/API#lookup-volume
+    #
+    # @param String volume_id
+    # @return Hash
+
     def dir_lookup(volume_id)
       res = Seaweed::HTTP.get "#{@base_url}/dir/lookup?volumeId=#{volume_id}"
       Seaweed::HTTP.parse(res)[:locations][0]
     end
+
+    # force garbage collection
+    # https://github.com/chrislusf/seaweedfs/wiki/API#force-garbage-collection
+    #
+    # @param Float threshold
 
     def vaccum!(threshold: 0.3)
       res = Seaweed::HTTP.get "#{@base_url}/vol/vaccum?garbageThreshold=#{threshold}"
