0f4e2ece66e1494849d42c0bab4398c43257e350
John Guest <guest.john88@gmail.com>
2015-09-25 14:22:17 -0700

    http documentation

--- lib/seaweed/http.rb
+++ lib/seaweed/http.rb
@@ -6,15 +6,25 @@ class Seaweed::HTTP
   class << self
     extend Forwardable
 
+    def_delegators :@http, :get, :put, :post, :delete
+
+    # inject your own rest client or json parser
+    #
+    # @param RestClient http
+    # @param JSON json
+
     def configure(http: RestClient, json: JSON)
       @http = http
       @json = json
     end
 
+    # JSON parse with default symbolized keys
+    #
+    # @param String json
+    # @return Hash
+
     def parse(json)
       @json.parse json, symbolize_names: true
     end
-
-    def_delegators :@http, :get, :put, :post, :delete
   end
 end
